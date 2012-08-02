ea63b2190c85066147ff3d0aab7d7ed33a6e7d46
Ignacio Sánchez <nacho@geardome.com>
2012-08-03 01:12:56 +0200

    Class SixteenBitRegister implemented

--- src/SixteenBitRegister.cpp
+++ src/SixteenBitRegister.cpp
@@ -4,11 +4,51 @@ SixteenBitRegister::SixteenBitRegister()
 {
 }
 
-SixteenBitRegister::SixteenBitRegister(const SixteenBitRegister& orig)
+void SixteenBitRegister::SetLow(u8 low)
 {
+    this->m_Low.SetValue(low);
 }
 
-SixteenBitRegister::~SixteenBitRegister()
+u8 SixteenBitRegister::GetLow() const
 {
+    return m_Low.GetValue();
+}
+
+void SixteenBitRegister::SetHigh(u8 high)
+{
+    this->m_High.SetValue(high);
+}
+
+u8 SixteenBitRegister::GetHigh() const
+{
+    return m_High.GetValue();
+}
+
+void SixteenBitRegister::SetValue(u16 value)
+{
+    m_Low.SetValue((u8) (value & BIT_MASK_8));
+    m_High.SetValue((u8) ((value >> 8) & BIT_MASK_8))
+}
+
+u16 SixteenBitRegister::GetValue() const
+{
+    u8 high = m_High.GetValue();
+    u8 low = m_Low.GetValue();
+
+    return (high << 8) +low;
+}
+
+void SixteenBitRegister::Increment()
+{
+    u16 value = this->GetValue();
+    value++;
+    this->SetValue(value);
+}
+
+void SixteenBitRegister::Decrement()
+{
+    u16 value = this->GetValue();
+    value--;
+    this->SetValue(value);
 }
 
--- src/SixteenBitRegister.h
+++ src/SixteenBitRegister.h
@@ -2,14 +2,23 @@
 #define	SIXTEENBITREGISTER_H
 
 #include "definitions.h"
+#include "EightBitRegister.h"
 
 class SixteenBitRegister
 {
 public:
     SixteenBitRegister();
-    SixteenBitRegister(const SixteenBitRegister& orig);
-    virtual ~SixteenBitRegister();
+    void SetLow(u8 low);
+    u8 GetLow() const;
+    void SetHigh(u8 high);
+    u8 GetHigh() const;
+    void SetValue(u16 value);
+    u16 GetValue() const;
+    void Increment();
+    void Decrement();
 private:
+    EightBitRegister m_High;
+    EightBitRegister m_Low;
 
 };
 
