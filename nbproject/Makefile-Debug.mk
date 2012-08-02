4adb37d7a973c5dabce67a2b730460ab770cfc75
Ignacio Sánchez <nacho@geardome.com>
2012-08-03 01:12:04 +0200

    Changes in project build

--- nbproject/Makefile-Debug.mk
+++ nbproject/Makefile-Debug.mk
@@ -35,8 +35,8 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
-	${OBJECTDIR}/src/EightBitRegister.o \
 	${OBJECTDIR}/src/main.o \
+	${OBJECTDIR}/src/EightBitRegister.o \
 	${OBJECTDIR}/src/MemoryRule.o \
 	${OBJECTDIR}/src/MemoryCell.o \
 	${OBJECTDIR}/src/Cartridge.o \
@@ -72,15 +72,15 @@ ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/gearboy: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/gearboy ${OBJECTFILES} ${LDLIBSOPTIONS} 
 
-${OBJECTDIR}/src/EightBitRegister.o: src/EightBitRegister.cpp 
-	${MKDIR} -p ${OBJECTDIR}/src
-	${RM} $@.d
-	$(COMPILE.cc) -g -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/EightBitRegister.o src/EightBitRegister.cpp
-
 ${OBJECTDIR}/src/main.o: src/main.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} $@.d
 	$(COMPILE.cc) -g -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/main.o src/main.cpp
+
+${OBJECTDIR}/src/EightBitRegister.o: src/EightBitRegister.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} $@.d
+	$(COMPILE.cc) -g -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/EightBitRegister.o src/EightBitRegister.cpp
 
 ${OBJECTDIR}/src/MemoryRule.o: src/MemoryRule.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
