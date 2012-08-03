e803cd350174ef2e0f512ded8caa0009ec3f3364
Ignacio Sánchez <nacho@geardome.com>
2012-08-03 02:10:21 +0200

    New definitions and changes

--- src/definitions.h
+++ src/definitions.h
@@ -12,10 +12,12 @@ typedef int16_t s16;
 #define BIT_MASK_8 0xFF
 #define BIT_MASK_16 0xFFFF
 
-#define FLAG_ZERO = 0x80
-#define FLAG_SUB = 0x40
-#define FLAG_HALF = 0x20
-#define FLAG_CARRY = 0x10
+#define FLAG_ZERO 0x80
+#define FLAG_SUB 0x40
+#define FLAG_HALF 0x20
+#define FLAG_CARRY 0x10
+
+#define MAX_STRING_SIZE 256
 
 #endif	/* DEFINITIONS_H */
 
