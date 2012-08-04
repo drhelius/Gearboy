a984f0b3ce6aecb4c2a35fc402bfc4e04e8c1c80
Ignacio Sánchez <nacho@geardome.com>
2012-08-05 01:36:09 +0200

    Added some flag operations

--- src/Processor.cpp
+++ src/Processor.cpp
@@ -8,3 +8,36 @@ Processor::~Processor()
 {
 }
 
+void Processor::ClearAllFlags()
+{
+    SetFlag(FLAG_NONE);
+}
+
+void Processor::ToggleZeroFlagFromResult(u8 result)
+{
+    if (result == 0)
+    {
+        ToggleFlag(FLAG_ZERO);
+    }
+}
+
+void Processor::SetFlag(u8 flag)
+{
+    AF.SetLow(flag);
+}
+
+void Processor::ToggleFlag(u8 flag)
+{
+    AF.SetLow(AF.GetLow() | flag);
+}
+
+void Processor::UntoggleFlag(u8 flag)
+{
+    AF.SetLow(AF.GetLow() & (~flag));
+}
+
+bool Processor::IsSetFlag(u8 flag)
+{
+    return (AF.GetLow() & flag);
+}
+
--- src/Processor.h
+++ src/Processor.h
@@ -9,6 +9,13 @@ class Processor
 public:
     Processor();
     ~Processor();
+    void ClearAllFlags();
+    void ToggleZeroFlagFromResult(u8 result);
+    void SetFlag(u8 flag);
+    void ToggleFlag(u8 flag);
+    void UntoggleFlag(u8 flag);
+    bool IsSetFlag(u8 flag);
+     
 private:
     SixteenBitRegister AF;
     SixteenBitRegister BC;
