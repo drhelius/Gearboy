57b1408398986947cbfe8db1dda91dff00331da1
Ignacio Sánchez <nacho@geardome.com>
2012-08-05 04:29:35 +0200

    Added helper functions to deal with opcodes

--- src/Processor.cpp
+++ src/Processor.cpp
@@ -1,7 +1,9 @@
 #include "Processor.h"
+#include "Memory.h"
 
-Processor::Processor()
+Processor::Processor(Memory* pMemory)
 {
+    m_pMemory = pMemory;
 }
 
 Processor::~Processor()
@@ -39,5 +41,465 @@ void Processor::UntoggleFlag(u8 flag)
 bool Processor::IsSetFlag(u8 flag)
 {
     return (AF.GetLow() & flag);
+}
+
+void Processor::OPCodes_LD(EightBitRegister* reg1, EightBitRegister reg2)
+{
+    reg1->SetValue(reg2.GetValue());
+}
+
+void Processor::OPCodes_LD(EightBitRegister* reg, u16 address)
+{
+    reg->SetValue(m_pMemory->Read(address));
+}
+
+void Processor::OPCodes_LD(u16 address, EightBitRegister reg)
+{
+    m_pMemory->Write(address, reg.GetValue());
+}
+
+void Processor::OPCodes_OR(u8 number)
+{
+    u8 result = AF.GetHigh() | number;
+    AF.SetHigh(result);
+    ClearAllFlags();
+    ToggleZeroFlagFromResult(result);
+}
+
+void Processor::OPCodes_XOR(u8 number)
+{
+    u8 result = AF.GetHigh() ^ number;
+    AF.SetHigh(result);
+    ClearAllFlags();
+    ToggleZeroFlagFromResult(result);
+}
+
+void Processor::OPCodes_AND(u8 number)
+{
+    u8 result = AF.GetHigh() & number;
+    AF.SetHigh(result);
+    SetFlag(FLAG_HALF);
+    ToggleZeroFlagFromResult(result);
+}
+
+void Processor::OPCodes_CP(u8 number)
+{
+    SetFlag(FLAG_SUB);
+
+    if (AF.GetHigh() < number)
+    {
+        ToggleFlag(FLAG_CARRY);
+    }
+
+    if (AF.GetHigh() == number)
+    {
+        ToggleFlag(FLAG_ZERO);
+    }
+
+    if (((AF.GetHigh() & 0x1F) - (number & 0x1F)) < 0x1F)
+    {
+        ToggleFlag(FLAG_HALF);
+    }
+}
+
+void Processor::OPCodes_INC(EightBitRegister* reg)
+{
+    u8 result = reg->GetValue() + 1;
+    reg->SetValue(result);
+    IsSetFlag(FLAG_CARRY) ? SetFlag(FLAG_CARRY) : ClearAllFlags();
+    ToggleZeroFlagFromResult(result);
+    if ((result & 0x0F) == 0x00)
+    {
+        ToggleFlag(FLAG_HALF);
+    }
+}
+
+void Processor::OPCodes_INC_HL()
+{
+    u8 result = m_pMemory->Read(HL.GetValue()) + 1;
+    m_pMemory->Write(HL.GetValue(), result);
+    IsSetFlag(FLAG_CARRY) ? SetFlag(FLAG_CARRY) : ClearAllFlags();
+    ToggleZeroFlagFromResult(result);
+    if ((result & 0x0F) == 0x00)
+    {
+        ToggleFlag(FLAG_HALF);
+    }
+}
+
+void Processor::OPCodes_DEC(EightBitRegister* reg)
+{
+    u8 result = reg->GetValue() - 1;
+    reg->SetValue(result);
+    IsSetFlag(FLAG_CARRY) ? SetFlag(FLAG_CARRY) : ClearAllFlags();
+    ToggleFlag(FLAG_SUB);
+    ToggleZeroFlagFromResult(result);
+    if ((result & 0x0F) == 0x0F)
+    {
+        ToggleFlag(FLAG_HALF);
+    }
+}
+
+void Processor::OPCodes_DEC_HL()
+{
+    u8 result = m_pMemory->Read(HL.GetValue()) - 1;
+    m_pMemory->Write(HL.GetValue(), result);
+    IsSetFlag(FLAG_CARRY) ? SetFlag(FLAG_CARRY) : ClearAllFlags();
+    ToggleFlag(FLAG_SUB);
+    ToggleZeroFlagFromResult(result);
+    if ((result & 0x0F) == 0x0F)
+    {
+        ToggleFlag(FLAG_HALF);
+    }
+}
+
+void Processor::OPCodes_ADD(u8 number)
+{
+    int result = AF.GetHigh() + number;
+    int carrybits = AF.GetHigh() ^ number ^ result;
+    AF.SetHigh(static_cast<u8> (result));
+    ClearAllFlags();
+    ToggleZeroFlagFromResult(static_cast<u8> (result));
+    if ((carrybits & 0x100) != 0)
+    {
+        ToggleFlag(FLAG_CARRY);
+    }
+    if ((carrybits & 0x10) != 0)
+    {
+        ToggleFlag(FLAG_HALF);
+    }
+}
+
+void Processor::OPCodes_ADC(u8 number)
+{
+    int carry = IsSetFlag(FLAG_CARRY) ? 1 : 0;
+    int result = AF.GetHigh() + number + carry;
+    int carrybits = AF.GetHigh() ^ (number + carry) ^ result;
+    AF.SetHigh(static_cast<u8> (result));
+    ClearAllFlags();
+    ToggleZeroFlagFromResult(static_cast<u8> (result));
+    if ((carrybits & 0x100) != 0)
+    {
+        ToggleFlag(FLAG_CARRY);
+    }
+    if ((carrybits & 0x10) != 0)
+    {
+        ToggleFlag(FLAG_HALF);
+    }
+}
+
+void Processor::OPCodes_SUB(u8 number)
+{
+    int result = AF.GetHigh() - number;
+    int carrybits = AF.GetHigh() ^ number ^ result;
+    AF.SetHigh(static_cast<u8> (result));
+    SetFlag(FLAG_SUB);
+    ToggleZeroFlagFromResult(static_cast<u8> (result));
+    if ((carrybits & 0x100) != 0)
+    {
+        ToggleFlag(FLAG_CARRY);
+    }
+    if ((carrybits & 0x10) != 0)
+    {
+        ToggleFlag(FLAG_HALF);
+    }
+}
+
+void Processor::OPCodes_SBC(u8 number)
+{
+    int carry = IsSetFlag(FLAG_CARRY) ? 1 : 0;
+    int result = AF.GetHigh() - (number + carry);
+    int carrybits = AF.GetHigh() ^ (number + carry) ^ result;
+    AF.SetHigh(static_cast<u8> (result));
+    SetFlag(FLAG_SUB);
+    ToggleZeroFlagFromResult(static_cast<u8> (result));
+    if ((carrybits & 0x100) != 0)
+    {
+        ToggleFlag(FLAG_CARRY);
+    }
+    if ((carrybits & 0x10) != 0)
+    {
+        ToggleFlag(FLAG_HALF);
+    }
+}
+
+void Processor::OPCodes_ADD_HL(u16 number)
+{
+    int result = HL.GetValue() + number;
+    IsSetFlag(FLAG_ZERO) ? SetFlag(FLAG_ZERO) : ClearAllFlags();
+    if (result & 0x10000)
+        ToggleFlag(FLAG_CARRY);
+    if ((HL.GetValue() ^ number ^ (result & 0xFFFF)) & 0x1000)
+        ToggleFlag(FLAG_HALF);
+    HL.SetValue(static_cast<u16> (result));
+}
+
+void Processor::OPCodes_ADD_SP(s8 number)
+{
+    int result = SP.GetValue() + number;
+    ClearAllFlags();
+    if (number >= 0)
+    {
+        if (SP.GetValue() > result)
+            ToggleFlag(FLAG_CARRY);
+        if (((SP.GetValue() ^ number ^ result) & 0x1000) != 0)
+            ToggleFlag(FLAG_HALF);
+    }
+    else
+    {
+        if (SP.GetValue() < result)
+            ToggleFlag(FLAG_CARRY);
+        if (((SP.GetValue() ^ number ^ result) & 0x1000) != 0)
+            ToggleFlag(FLAG_HALF);
+    }
+    SP.SetValue(static_cast<u16> (result));
+}
+
+void Processor::OPCodes_SWAP_Register(EightBitRegister* reg)
+{
+    u8 low_half = reg->GetValue() & 0x0F;
+    u8 high_half = (reg->GetValue() >> 4) & 0x0F;
+    reg->SetValue((low_half << 4) + high_half);
+    ClearAllFlags();
+    ToggleZeroFlagFromResult(reg->GetValue());
+}
+
+void Processor::OPCodes_SWAP_HL()
+{
+    u8 number = m_pMemory->Read(HL.GetValue());
+    u8 low_half = number & 0x0F;
+    u8 high_half = (number >> 4) & 0x0F;
+    number = (low_half << 4) + high_half;
+    m_pMemory->Write(HL.GetValue(), number);
+    ClearAllFlags();
+    ToggleZeroFlagFromResult(number);
+}
+
+void Processor::OPCodes_SLA(EightBitRegister* reg)
+{
+    (reg->GetValue() & 0x80) != 0 ? SetFlag(FLAG_CARRY) : ClearAllFlags();
+    u8 result = reg->GetValue() << 1;
+    reg->SetValue(result);
+    ToggleZeroFlagFromResult(result);
+}
+
+void Processor::OPCodes_SLA_HL()
+{
+    u8 result = m_pMemory->Read(HL.GetValue());
+    (result & 0x80) != 0 ? SetFlag(FLAG_CARRY) : ClearAllFlags();
+    result <<= 1;
+    m_pMemory->Write(HL.GetValue(), result);
+    ToggleZeroFlagFromResult(result);
+}
+
+void Processor::OPCodes_SRA(EightBitRegister* reg)
+{
+    u8 result = reg->GetValue();
+    (result & 0x01) != 0 ? SetFlag(FLAG_CARRY) : ClearAllFlags();
+    if ((result & 0x80) != 0)
+    {
+        result >>= 1;
+        result |= 0x80;
+    }
+    else
+    {
+        result >>= 1;
+    }
+    reg->SetValue(result);
+    ToggleZeroFlagFromResult(result);
+}
+
+void Processor::OPCodes_SRA_HL()
+{
+    u8 result = m_pMemory->Read(HL.GetValue());
+    (result & 0x01) != 0 ? SetFlag(FLAG_CARRY) : ClearAllFlags();
+    if ((result & 0x80) != 0)
+    {
+        result >>= 1;
+        result |= 0x80;
+    }
+    else
+    {
+        result >>= 1;
+    }
+    m_pMemory->Write(HL.GetValue(), result);
+    ToggleZeroFlagFromResult(result);
+}
+
+void Processor::OPCodes_SRL(EightBitRegister* reg)
+{
+    u8 result = reg->GetValue();
+    (result & 0x01) != 0 ? SetFlag(FLAG_CARRY) : ClearAllFlags();
+    result >>= 1;
+    reg->SetValue(result);
+    ToggleZeroFlagFromResult(result);
+}
+
+void Processor::OPCodes_SRL_HL()
+{
+    u8 result = m_pMemory->Read(HL.GetValue());
+    (result & 0x01) != 0 ? SetFlag(FLAG_CARRY) : ClearAllFlags();
+    result >>= 1;
+    m_pMemory->Write(HL.GetValue(), result);
+    ToggleZeroFlagFromResult(result);
+}
+
+void Processor::OPCodes_RLC(EightBitRegister* reg)
+{
+    u8 result = reg->GetValue();
+    if ((result & 0x80) != 0)
+    {
+        SetFlag(FLAG_CARRY);
+        result <<= 1;
+        result |= 0x1;
+    }
+    else
+    {
+        ClearAllFlags();
+        result <<= 1;
+    }
+    reg->SetValue(result);
+    ToggleZeroFlagFromResult(result);
+}
+
+void Processor::OPCodes_RLC_HL()
+{
+    u8 result = m_pMemory->Read(HL.GetValue());
+    if ((result & 0x80) != 0)
+    {
+        SetFlag(FLAG_CARRY);
+        result <<= 1;
+        result |= 0x1;
+    }
+    else
+    {
+        ClearAllFlags();
+        result <<= 1;
+    }
+    m_pMemory->Write(HL.GetValue(), result);
+    ToggleZeroFlagFromResult(result);
+}
+
+void Processor::OPCodes_RL(EightBitRegister* reg)
+{
+    u8 carry = IsSetFlag(FLAG_CARRY) ? 1 : 0;
+    u8 result = reg->GetValue();
+    ((result & 0x80) != 0) ? SetFlag(FLAG_CARRY) : ClearAllFlags();
+    result <<= 1;
+    result |= carry;
+    reg->SetValue(result);
+    ToggleZeroFlagFromResult(result);
+}
+
+void Processor::OPCodes_RL_HL()
+{
+    u8 carry = IsSetFlag(FLAG_CARRY) ? 1 : 0;
+    u8 result = m_pMemory->Read(HL.GetValue());
+    ((result & 0x80) != 0) ? SetFlag(FLAG_CARRY) : ClearAllFlags();
+    result <<= 1;
+    result |= carry;
+    m_pMemory->Write(HL.GetValue(), result);
+    ToggleZeroFlagFromResult(result);
+}
+
+void Processor::OPCodes_RRC(EightBitRegister* reg)
+{
+    u8 result = reg->GetValue();
+    if ((result & 0x01) != 0)
+    {
+        SetFlag(FLAG_CARRY);
+        result >>= 1;
+        result |= 0x80;
+    }
+    else
+    {
+        ClearAllFlags();
+        result >>= 1;
+    }
+    reg->SetValue(result);
+    ToggleZeroFlagFromResult(result);
+}
+
+void Processor::OPCodes_RRC_HL()
+{
+    u8 result = m_pMemory->Read(HL.GetValue());
+    if ((result & 0x01) != 0)
+    {
+        SetFlag(FLAG_CARRY);
+        result >>= 1;
+        result |= 0x80;
+    }
+    else
+    {
+        ClearAllFlags();
+        result >>= 1;
+    }
+    m_pMemory->Write(HL.GetValue(), result);
+    ToggleZeroFlagFromResult(result);
+}
+
+void Processor::OPCodes_RR(EightBitRegister* reg)
+{
+    u8 carry = IsSetFlag(FLAG_CARRY) ? 0x80 : 0x00;
+    u8 result = reg->GetValue();
+    ((result & 0x01) != 0) ? SetFlag(FLAG_CARRY) : ClearAllFlags();
+    result >>= 1;
+    result |= carry;
+    reg->SetValue(result);
+    ToggleZeroFlagFromResult(result);
+}
+
+void Processor::OPCodes_RR_HL()
+{
+    u8 carry = IsSetFlag(FLAG_CARRY) ? 0x80 : 0x00;
+    u8 result = m_pMemory->Read(HL.GetValue());
+    ((result & 0x01) != 0) ? SetFlag(FLAG_CARRY) : ClearAllFlags();
+    result >>= 1;
+    result |= carry;
+    m_pMemory->Write(HL.GetValue(), result);
+    ToggleZeroFlagFromResult(result);
+}
+
+void Processor::OPCodes_BIT(EightBitRegister* reg, int bit)
+{
+    if (((reg->GetValue() >> bit) & 0x01) == 0)
+        ToggleFlag(FLAG_ZERO);
+    else
+        UntoggleFlag(FLAG_ZERO);
+    ToggleFlag(FLAG_HALF);
+    UntoggleFlag(FLAG_SUB);
+}
+
+void Processor::OPCodes_BIT_HL(int bit)
+{
+    if (((m_pMemory->Read(HL.GetValue()) >> bit) & 0x01) == 0)
+        ToggleFlag(FLAG_ZERO);
+    else
+        UntoggleFlag(FLAG_ZERO);
+    ToggleFlag(FLAG_HALF);
+    UntoggleFlag(FLAG_SUB);
+}
+
+void Processor::OPCodes_SET(EightBitRegister* reg, int bit)
+{
+    reg->SetValue(reg->GetValue() | (0x1 << bit));
+}
+
+void Processor::OPCodes_SET_HL(int bit)
+{
+    u8 result = m_pMemory->Read(HL.GetValue());
+    result |= (0x1 << bit);
+    m_pMemory->Write(HL.GetValue(), result);
+}
+
+void Processor::OPCodes_RES(EightBitRegister* reg, int bit)
+{
+    reg->SetValue(reg->GetValue() & (~(0x1 << bit)));
+}
+
+void Processor::OPCodes_RES_HL(int bit)
+{
+    u8 result = m_pMemory->Read(HL.GetValue());
+    result &= ~(0x1 << bit);
+    m_pMemory->Write(HL.GetValue(), result);
 }
 
--- src/Processor.h
+++ src/Processor.h
@@ -4,19 +4,63 @@
 #include "definitions.h"
 #include "SixteenBitRegister.h"
 
+class Memory;
+
 class Processor
 {
 public:
-    Processor();
+    Processor(Memory* pMemory);
     ~Processor();
+    
+private:
     void ClearAllFlags();
     void ToggleZeroFlagFromResult(u8 result);
     void SetFlag(u8 flag);
     void ToggleFlag(u8 flag);
     void UntoggleFlag(u8 flag);
     bool IsSetFlag(u8 flag);
-     
+    void OPCodes_LD(EightBitRegister* reg1, EightBitRegister reg2);
+    void OPCodes_LD(EightBitRegister* reg, u16 address);
+    void OPCodes_LD(u16 address, EightBitRegister reg);
+    void OPCodes_OR(u8 number);
+    void OPCodes_XOR(u8 number);
+    void OPCodes_AND(u8 number);
+    void OPCodes_CP(u8 number);
+    void OPCodes_INC(EightBitRegister* reg);
+    void OPCodes_INC_HL();
+    void OPCodes_DEC(EightBitRegister* reg);
+    void OPCodes_DEC_HL();
+    void OPCodes_ADD(u8 number);
+    void OPCodes_ADC(u8 number);
+    void OPCodes_SUB(u8 number);
+    void OPCodes_SBC(u8 number);
+    void OPCodes_ADD_HL(u16 number);
+    void OPCodes_ADD_SP(s8 number);
+    void OPCodes_SWAP_Register(EightBitRegister* reg);
+    void OPCodes_SWAP_HL();
+    void OPCodes_SLA(EightBitRegister* reg);
+    void OPCodes_SLA_HL();
+    void OPCodes_SRA(EightBitRegister* reg);
+    void OPCodes_SRA_HL();
+    void OPCodes_SRL(EightBitRegister* reg);
+    void OPCodes_SRL_HL();
+    void OPCodes_RLC(EightBitRegister* reg);
+    void OPCodes_RLC_HL();
+    void OPCodes_RL(EightBitRegister* reg);
+    void OPCodes_RL_HL();
+    void OPCodes_RRC(EightBitRegister* reg);
+    void OPCodes_RRC_HL();
+    void OPCodes_RR(EightBitRegister* reg);
+    void OPCodes_RR_HL();
+    void OPCodes_BIT(EightBitRegister* reg, int bit);
+    void OPCodes_BIT_HL(int bit);
+    void OPCodes_SET(EightBitRegister* reg, int bit);
+    void OPCodes_SET_HL(int bit);
+    void OPCodes_RES(EightBitRegister* reg, int bit);
+    void OPCodes_RES_HL(int bit);
+
 private:
+    Memory* m_pMemory;
     SixteenBitRegister AF;
     SixteenBitRegister BC;
     SixteenBitRegister DE;
