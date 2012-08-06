78c17a2839e49a0a7586563e489e9eed75bd6517
Ignacio Sánchez <nacho@geardome.com>
2012-08-06 23:33:03 +0200

    test function

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
 #include "gearboy.h"
+#include "Memory.h"
+#include "Processor.h"
+
+
+void testfunc()
+{
+    Memory* mem = new Memory();
+    Processor* cpu = new Processor(mem);
+    
+    cpu->PC.SetValue(10);
+    
+    SafeDelete(cpu);
+    SafeDelete(mem);
+}
 
 int main(int argc, char** argv)
 {
-
+    testfunc();
     return 0;
 }
+
 
