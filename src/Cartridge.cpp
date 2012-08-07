efed2c5cf7b5f52b9aa7dc458a82d13ad6600b25
Ignacio Sánchez <nacho@geardome.com>
2012-08-08 01:15:04 +0200

    added cartridge operations

--- src/Cartridge.cpp
+++ src/Cartridge.cpp
@@ -2,13 +2,129 @@
 
 Cartridge::Cartridge()
 {
+    Reset();
 }
 
 Cartridge::~Cartridge()
 {
+    SafeDeleteArray(m_pTheROM);
 }
 
 void Cartridge::Reset()
 {
+    SafeDeleteArray(m_pTheROM);
+    m_iTotalSize = 0;
+    m_szName[0] = 0;
+    m_iROMSize = 0;
+    m_iRAMSize = 0;
+    m_iType = 0;
+    m_bValidROM = false;
+}
+
+bool Cartridge::IsValidROM() const
+{
+    return m_bValidROM;
+}
+
+int Cartridge::GetType() const
+{
+    return m_iType;
+}
+
+int Cartridge::GetRAMSize() const
+{
+    return m_iRAMSize;
+}
+
+int Cartridge::GetROMSize() const
+{
+    return m_iROMSize;
+}
+
+const char* Cartridge::GetName() const
+{
+    return m_szName;
+}
+
+int Cartridge::GetTotalSize() const
+{
+    return m_iTotalSize;
+}
+
+u8* Cartridge::GetTheROM() const
+{
+    return m_pTheROM;
+}
+
+void Cartridge::LoadFromFile(const char* path)
+{
+    using namespace std;
+
+    ifstream file(path, ios::in | ios::binary | ios::ate);
+
+    if (file.is_open())
+    {
+        ifstream::pos_type size = file.tellg();
+        char* memblock = new char[size];
+        file.seekg(0, ios::beg);
+        file.read(memblock, size);
+        file.close();
+
+        LoadFromBuffer(reinterpret_cast<u8*> (memblock), size);
+
+        SafeDeleteArray(memblock);
+    }
+    else
+    {
+        //error
+    }
+}
+
+void Cartridge::LoadFromBuffer(const u8* buffer, int size)
+{
+    if (IsValidPointer(buffer))
+    {
+        m_iTotalSize = size;
+        m_pTheROM = new u8[m_iTotalSize];
+        memcpy(m_pTheROM, buffer, m_iTotalSize);
+
+        GatherMetadata();
+    }
+}
+
+void Cartridge::GatherMetadata()
+{
+    char name[16] = {0};
+
+    for (int i = 0; i < 16; i++)
+    {
+        name[i] = m_pTheROM[0x0134 + i];
+        
+        if (name[i] == 0)
+        {
+            break;
+        }
+    }
+    
+    strcpy(m_szName, name);
+
+    m_iType = m_pTheROM[0x147];
+    m_iROMSize = m_pTheROM[0x148];
+    m_iRAMSize = m_pTheROM[0x149];
+
+    int checksum = 0;
+
+    for (int j = 0x134; j < 0x14E; j++)
+    {
+        checksum += m_pTheROM[j];
+    }
+
+    m_bValidROM = ((checksum + 25) & BIT_MASK_8) == 0;
+
+    //  only accept 32kb cartridges until mbc code is written
+    if ((m_iType != 0) || (m_iROMSize != 0) || (m_iRAMSize != 0))
+    {
+        m_bValidROM = false;
+    }
 }
 
--- src/Cartridge.h
+++ src/Cartridge.h
@@ -9,8 +9,25 @@ public:
     Cartridge();
     ~Cartridge();
     void Reset();
+    bool IsValidROM() const;
+    int GetType() const;
+    int GetRAMSize() const;
+    int GetROMSize() const;
+    const char* GetName() const;
+    int GetTotalSize() const;
+    u8* GetTheROM() const;
+    void LoadFromFile(const char* path);
+    void LoadFromBuffer(const u8* buffer, int size);
 private:
-
+    void GatherMetadata();
+private:
+    u8* m_pTheROM;
+    int m_iTotalSize;
+    char m_szName[MAX_STRING_SIZE];
+    int m_iROMSize;
+    int m_iRAMSize;
+    int m_iType;
+    bool m_bValidROM;
 };
 
 #endif	/* CARTRIDGE_H */
