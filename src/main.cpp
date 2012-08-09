ac6aac7b214c685e717212dc31e5b4f9abb2f5f3
Ignacio Sánchez <nacho@geardome.com>
2012-08-09 03:01:40 +0200

    basic OpenGL working

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include <GLUT/glut.h>
 #else
 #ifdef _WIN32
-  #include <windows.h>
+#include <windows.h>
 #endif
 #include <GL/gl.h>
 #include <GL/glu.h>
@@ -19,104 +19,102 @@ const int modifier = 4;
 
 int display_width = SCREEN_WIDTH * modifier;
 int display_height = SCREEN_HEIGHT * modifier;
+u8 screenData[SCREEN_HEIGHT][SCREEN_WIDTH][3];
+u8* frameBuffer;
+Core* gb;
 
 void setupTexture()
 {
     // Clear screen
-    for(int y = 0; y < SCREEN_HEIGHT; ++y)         
-        for(int x = 0; x < SCREEN_WIDTH; ++x)
+    for (int y = 0; y < SCREEN_HEIGHT; ++y)
+        for (int x = 0; x < SCREEN_WIDTH; ++x)
             screenData[y][x][0] = screenData[y][x][1] = screenData[y][x][2] = 0;
- 
+
     // Create a texture 
-    glTexImage2D(GL_TEXTURE_2D, 0, 3, SCREEN_WIDTH, SCREEN_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid*)screenData);
- 
+    glTexImage2D(GL_TEXTURE_2D, 0, 3, SCREEN_WIDTH, SCREEN_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid*) screenData);
+
     // Set up the texture
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP); 
- 
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+
     // Enable textures
     glEnable(GL_TEXTURE_2D);
 }
- 
-void updateTexture(const chip8& c8)
-{     
+
+void updateTexture()
+{
     // Update pixels
-    for(int y = 0; y < 32; ++y)               
-        for(int x = 0; x < 64; ++x)
-            if(c8.gfx[(y * 64) + x] == 0)
-                screenData[y][x][0] = screenData[y][x][1] = screenData[y][x][2] = 0;  // Disabled
-            else 
-                screenData[y][x][0] = screenData[y][x][1] = screenData[y][x][2] = 255;  // Enabled
- 
+    for (int y = 0; y < SCREEN_HEIGHT; ++y)
+        for (int x = 0; x < SCREEN_WIDTH; ++x)
+            if (frameBuffer[(y * 64) + x] == 0)
+                screenData[y][x][0] = screenData[y][x][1] = screenData[y][x][2] = 0; // Disabled
+            else
+                screenData[y][x][0] = screenData[y][x][1] = screenData[y][x][2] = 255; // Enabled
+
     // Update Texture
-    glTexSubImage2D(GL_TEXTURE_2D, 0 ,0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid*)screenData);
- 
-    glBegin( GL_QUADS );
-        glTexCoord2d(0.0, 0.0);          glVertex2d(0.0,                      0.0);
-        glTexCoord2d(1.0, 0.0);  glVertex2d(display_width, 0.0);
-        glTexCoord2d(1.0, 1.0);  glVertex2d(display_width, display_height);
-        glTexCoord2d(0.0, 1.0);  glVertex2d(0.0,                      display_height);
+    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid*) screenData);
+
+    glBegin(GL_QUADS);
+    glTexCoord2d(0.0, 0.0);
+    glVertex2d(0.0, 0.0);
+    glTexCoord2d(1.0, 0.0);
+    glVertex2d(display_width, 0.0);
+    glTexCoord2d(1.0, 1.0);
+    glVertex2d(display_width, display_height);
+    glTexCoord2d(0.0, 1.0);
+    glVertex2d(0.0, display_height);
     glEnd();
 }
- 
+
 void display()
 {
-    myChip8.emulateCycle();
- 
-    if(myChip8.drawFlag)
-    {
-        // Clear framebuffer
-        glClear(GL_COLOR_BUFFER_BIT);
- 
-        // Draw pixels to texture
-        updateTexture(myChip8);
- 
-        // Swap buffers!
-        glutSwapBuffers();    
- 
-        // Processed frame
-        myChip8.drawFlag = false;
-        }
+    gb->RunToVBlank(frameBuffer);
+
+    glClear(GL_COLOR_BUFFER_BIT);
+    updateTexture();
+    glutSwapBuffers();
 }
- 
+
 void reshape_window(GLsizei w, GLsizei h)
 {
     glClearColor(0.0f, 0.0f, 0.5f, 0.0f);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluOrtho2D(0, w, h, 0);        
+    gluOrtho2D(0, w, h, 0);
     glMatrixMode(GL_MODELVIEW);
     glViewport(0, 0, w, h);
- 
+
     // Resize quad
     display_width = w;
     display_height = h;
 }
 
-
 int main(int argc, char** argv)
 {
-    Core* gb = new Core();
+    gb = new Core();
 
-    glutInit(&argc, argv);          
+    frameBuffer = new u8[SCREEN_WIDTH * SCREEN_HEIGHT];
+
+    glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
- 
+
     glutInitWindowSize(1024, 1024);
     glutInitWindowPosition(320, 320);
-    glutCreateWindow();
- 
+    glutCreateWindow("Gearboy");
+
     glutDisplayFunc(display);
     glutIdleFunc(display);
     glutReshapeFunc(reshape_window);
- 
+
     setupTexture();
- 
-    glutMainLoop(); 
-    
+
+    glutMainLoop();
+
+    SafeDeleteArray(frameBuffer);
     SafeDelete(gb);
-    
+
     return 0;
 }
 
