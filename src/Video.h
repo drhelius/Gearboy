89c77c1798a5e8c2acaffa5688f7005bdd3179bb
Ignacio Sánchez <nacho@geardome.com>
2012-08-16 04:23:35 +0200

    priority of backgrounds and sprites improved

--- src/Video.h
+++ src/Video.h
@@ -1,6 +1,7 @@
 #ifndef VIDEO_H
 #define	VIDEO_H
 
+#include <vector>
 #include "definitions.h"
 
 class Memory;
@@ -10,6 +11,7 @@ class Video
 {
 public:
     Video(Memory* pMemory, Processor* pProcessor);
+    ~Video();
     void Init();
     void Reset();
     bool Tick(u8 clockCycles, u8* pFrameBuffer);
@@ -21,18 +23,32 @@ private:
     void RenderBG(int line);
     void RenderWindow(int line);
     void RenderSprites(int line);
+    void SortSprites();
     void UpdateStatRegister();
     void UpdateLYRegister();
 private:
     Memory* m_pMemory;
     Processor* m_pProcessor;
     u8* m_pFrameBuffer;
+    u8* m_pBackgroundColorBuffer;
     int m_iStatusMode;
     int m_iStatusModeCounter;
     int m_iStatusModeCounterAux;
     int m_iStatusModeLYCounter;
     bool m_bScreenEnabled;
+    std::vector< std::pair<int, int> > m_SortedSpritesVector;
 };
 
+struct sprite_sort_x {
+    bool operator()(const std::pair<int,int> &left, const std::pair<int,int> &right) {
+        return left.second > right.second;
+    }
+};
+
+struct sprite_sort_oam {
+    bool operator()(const std::pair<int,int> &left, const std::pair<int,int> &right) {
+        return left.first > right.first;
+    }
+};
 #endif	/* VIDEO_H */
 
