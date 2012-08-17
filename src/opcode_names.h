3e4796d96bd30498e1893af3fb0c3928b2e4dd2c
Ignacio Sánchez <nacho@geardome.com>
2012-08-17 02:32:42 +0200

    added GPL license

--- src/opcode_names.h
+++ src/opcode_names.h
@@ -1,3 +1,19 @@
+/* 
+ * Gearboy Gameboy Emulator
+ * Copyright (C) 2012 Ignacio Sanchez
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ * The full license is available at http://www.gnu.org/licenses/gpl.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ */
+
 #ifndef OPCODE_NAMES_H
 #define	OPCODE_NAMES_H
 
