b9d2c649d016e69135e1459568ff8bb7834e4e7f
Ignacio Sánchez <nacho@geardome.com>
2012-08-21 21:16:21 +0200

    fixes OpenGL include files in multiple platforms

--- platforms/qt-shared/RenderThread.cpp
+++ platforms/qt-shared/RenderThread.cpp
@@ -14,7 +14,19 @@
  *
  */
 
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#else
+#ifdef _WIN32
+#include <windows.h>
+#endif
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#endif
+
 #include "RenderThread.h"
 #include "GLFrame.h"
 
