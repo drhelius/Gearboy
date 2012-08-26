3e7d49caa8e329bfb6fc05ae877deff27b3013cc
Ignacio Sánchez <nacho@geardome.com>
2012-08-27 01:45:36 +0200

    added license

--- platforms/qt-shared/Emulator.cpp
+++ platforms/qt-shared/Emulator.cpp
@@ -1,17 +1,20 @@
-/* 
- * Gearboy Gameboy Emulator
- * Copyright (C) 2012 Ignacio Sanchez
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation.
- * The full license is available at http://www.gnu.org/licenses/gpl.html
- *
+/*
+ * Gearboy - Nintendo Game Boy Emulator
+ * Copyright (C) 2012  Ignacio Sanchez
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/ 
+ * 
  */
 
 #include "Emulator.h"
