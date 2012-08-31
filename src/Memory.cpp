d029099453d7734f1ce4a44799da88cb4264c991
drhelius <nacho@geardome.com>
2012-08-31 14:49:12 +0200

    improved initial values on reset

--- src/Memory.cpp
+++ src/Memory.cpp
@@ -61,14 +61,70 @@ void Memory::Reset(bool bCGB)
     {
         m_pMap[i].Reset();
 
-        if ((i >= 0xA000) && (i < 0xC000))
+        if ((i >= 0x8000) && (i < 0xA000))
         {
-            m_pMap[i].SetValue(0xFF);
+            m_pMap[i].SetValue(0x00);
+            m_pLCDRAMBank1[i - 0x8000] = 0x00;
+        }
+        else if ((i >= 0xC000) && (i < 0xE000))
+        {
+            if ((i & 0x8) ^((i & 0x800)>>8))
+            {
+                if (m_bCGB)
+                {
+                    m_pMap[i].SetValue(0x00);
+                    if (i >= 0xD000)
+                    {
+                        for (int a=0; a < 8; a++)
+                        {
+                            if (a != 2)
+                                m_pWRAMBanks[(i - 0xD000) + (0x1000 * a)] = m_pMap[i - 0x1000].GetValue();
+                            else
+                                m_pWRAMBanks[(i - 0xD000) + (0x1000 * a)] = 0x00;
+                        }
+                    }
+                }
+                else
+                    m_pMap[i].SetValue(0x0f);
+            }
+            else
+            {
+                m_pMap[i].SetValue(0xff);
+                if (i >= 0xD000)
+                {
+                    for (int a=0; a < 8; a++)
+                    {
+                        if (a != 2)
+                            m_pWRAMBanks[(i - 0xD000) + (0x1000 * a)] = m_pMap[i - 0x1000].GetValue();
+                        else
+                            m_pWRAMBanks[(i - 0xD000) + (0x1000 * a)] = 0x00;
+                    }
+                }
+            }
         }
         else if (i >= 0xFF00)
         {
             m_pMap[i].SetValue(kInitialValuesForFFXX[i - 0xFF00]);
         }
+        else
+        {
+            m_pMap[i].SetValue(0xFF);
+        }
+    }
+
+    if (m_bCGB)
+    {
+        m_pMap[0xff02].SetValue(0x7c);
+        m_pMap[0xff56].SetValue(0x3e);
+        m_pMap[0xff4d].SetValue(0x7e);
+    }
+    else
+    {
+        m_pMap[0xff02].SetValue(0x7e);
+        m_pMap[0xff56].SetValue(0xff);
+        m_pMap[0xff4d].SetValue(0xff);
+        m_pMap[0xff70].SetValue(0xff);
+        m_pMap[0xff74].SetValue(0xff);
     }
 }
 
