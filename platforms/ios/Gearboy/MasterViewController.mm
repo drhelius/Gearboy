9c8242a8246564fed44ec82fca35c5943049c0f4
Ignacio Sánchez <nacho@geardome.com>
2012-09-09 01:43:32 +0200

    added license text

--- platforms/ios/Gearboy/MasterViewController.mm
+++ platforms/ios/Gearboy/MasterViewController.mm
@@ -1,10 +1,21 @@
-//
-//  MasterViewController.m
-//  Gearboy
-//
-//  Created by Ignacio Sánchez Ginés on 06/09/12.
-//  Copyright (c) 2012 Ignacio Sánchez Ginés. All rights reserved.
-//
+/*
+ * Gearboy - Nintendo Game Boy Emulator
+ * Copyright (C) 2012  Ignacio Sanchez
+ 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ *
+ */
 
 #import "MasterViewController.h"
 
