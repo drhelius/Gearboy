ea78aa2056f01141521d12b20ae52540b4fd6ee0
Ignacio Sánchez <nacho@geardome.com>
2012-09-16 02:21:25 +0200

    fixes a crash when going inactive

--- platforms/ios/Gearboy/AppDelegate.mm
+++ platforms/ios/Gearboy/AppDelegate.mm
@@ -65,6 +65,7 @@
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     self.detailViewController.theGLViewController.paused = YES;
+    [NSThread sleepForTimeInterval:1.5];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
