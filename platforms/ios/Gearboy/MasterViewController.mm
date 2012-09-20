0d8e3d4edba03a722f2a2a630a2ac3afb0415069
Ignacio Sánchez <nacho@geardome.com>
2012-09-21 01:28:26 +0200

    project updated to iOS 6 and iPhone 5

--- platforms/ios/Gearboy/MasterViewController.mm
+++ platforms/ios/Gearboy/MasterViewController.mm
@@ -24,6 +24,7 @@
 @implementation MasterViewController
 
 @synthesize listData;
+@synthesize sections = _sections;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
