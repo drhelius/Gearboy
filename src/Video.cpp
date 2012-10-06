9c4fe18af247b250b42247125aa31e9e7bb30613
Ignacio Sánchez <nacho@geardome.com>
2012-10-07 01:47:42 +0200

    scan line delayed 48 cycles, fixes many flashing lines

--- src/Video.cpp
+++ src/Video.cpp
@@ -221,12 +221,11 @@ bool Video::Tick(u8 clockCycles, GB_Color* pColorFrameBuffer)
                 if (!m_bScanLineTransfered && (m_iStatusModeCounter >= 48))
                 {
                     m_bScanLineTransfered = true;
-                    //ScanLine(m_iStatusModeLYCounter);
+                    ScanLine(m_iStatusModeLYCounter);
                 }
 
                 if (m_iStatusModeCounter >= 172)
                 {
-                    ScanLine(m_iStatusModeLYCounter);
                     m_iStatusModeCounter -= 172;
                     m_iStatusMode = 0;
                     UpdateStatRegister();
