67b4ff9dadf6099d656b7908124b55498ac313ec
Ignacio Sánchez Ginés <nacho@geardome.com>
2013-07-16 01:30:31 +0200

    Open ROM files from Safari and other apps like DropBox

--- platforms/ios/Gearboy/AppDelegate.mm
+++ platforms/ios/Gearboy/AppDelegate.mm
@@ -92,7 +92,31 @@
 -(BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
 {
     NSLog(@"open file %@", url);
-    return YES;
+    
+    if (url != nil && [url isFileURL])
+    {
+        NSString *documentsDirectory = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+        NSError* error;
+        NSFileManager *fileManager = [[NSFileManager alloc] init];
+        
+        NSString* srcPath = [url path];
+        NSString* destPath = [NSString stringWithFormat:@"%@/%@", documentsDirectory, [srcPath lastPathComponent]];
+        
+        NSLog(@"srcPath %@", srcPath);
+        NSLog(@"destPath %@", destPath);
+
+        
+        if ([fileManager copyItemAtPath:srcPath toPath:destPath error:&error])
+        {
+            [masterViewController loadWithROM:[[url path] lastPathComponent]];
+                
+            return YES;
+        }
+        
+        NSLog(@"%@", [error localizedDescription]);
+    }
+    
+    return NO;
 }
 
 @end
