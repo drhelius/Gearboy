b78ad0b3a393c604ccb5049364576dd9f19dd304
Ignacio Sánchez Ginés <nacho@geardome.com>
2013-07-17 20:37:15 +0200

    minor typo

--- platforms/ios/Gearboy/AppDelegate.mm
+++ platforms/ios/Gearboy/AppDelegate.mm
@@ -89,7 +89,7 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
--(BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
 {
     if (url != nil && [url isFileURL])
     {
