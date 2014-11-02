4673ad301be494349e39eb22dde339988fdc2e14
Ignacio Sanchez Gines <nacho@geardome.com>
2014-11-02 20:19:35 +0100

    fixed 64 bit warning

--- platforms/ios/Gearboy/inputmanager.mm
+++ platforms/ios/Gearboy/inputmanager.mm
@@ -72,7 +72,7 @@ void InputManager::AddRectRegionEvent(float x, float y, float width, float heigh
 
 void InputManager::ClearRegionEvents(void)
 {
-    int size = m_RegionEventVector.size();
+    int size = (int)m_RegionEventVector.size();
 
     for (int i = 0; i < size; i++)
     {
