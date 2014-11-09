a252c62d86ac074bcebf5bca68018ed940c5b940
Ignacio Sanchez Gines <nacho@geardome.com>
2014-11-09 01:03:17 +0100

    removed temp project

--- platforms/ios/Gearboy/main.m
+++ platforms/ios/Gearboy/main.m
@@ -1,28 +1,15 @@
-/*
- * Gearboy - Nintendo Game Boy Emulator
- * Copyright (C) 2012  Ignacio Sanchez
- 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see http://www.gnu.org/licenses/
- *
- */
+//
+//  main.m
+//  Gearboy
+//
+//  Created by Ignacio Sánchez Ginés on 04/10/14.
+//  Copyright (c) 2014 Ignacio Sánchez Ginés. All rights reserved.
+//
 
 #import <UIKit/UIKit.h>
-
 #import "AppDelegate.h"
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char * argv[]) {
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
