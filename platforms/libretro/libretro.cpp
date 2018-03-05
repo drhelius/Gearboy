8289359c585b0c319228b8b38002860458f74081
Ignacio Sanchez Gines <nacho@geardome.com>
2018-03-05 22:32:29 +0100

    memory access and achievements

--- platforms/libretro/libretro.cpp
+++ platforms/libretro/libretro.cpp
@@ -36,7 +36,7 @@
 
 #define VIDEO_WIDTH 160
 #define VIDEO_HEIGHT 144
-#define VIDEO_PIXELS VIDEO_WIDTH * VIDEO_HEIGHT
+#define VIDEO_PIXELS (VIDEO_WIDTH * VIDEO_HEIGHT)
 
 GB_Color *gearboy_frame_buf;
 
@@ -50,11 +50,11 @@ static int audio_sample_count;
 
 static void fallback_log(enum retro_log_level level, const char *fmt, ...)
 {
-   (void)level;
-   va_list va;
-   va_start(va, fmt);
-   vfprintf(stderr, fmt, va);
-   va_end(va);
+    (void)level;
+    va_list va;
+    va_start(va, fmt);
+    vfprintf(stderr, fmt, va);
+    va_end(va);
 }
 
 GearboyCore* core;
@@ -63,44 +63,44 @@ static retro_environment_t environ_cb;
 
 void retro_init(void)
 {
-   const char *dir = NULL;
+    const char *dir = NULL;
 
-   if (environ_cb(RETRO_ENVIRONMENT_GET_SYSTEM_DIRECTORY, &dir) && dir)
-   {
-      snprintf(retro_base_directory, sizeof(retro_base_directory), "%s", dir);
-   }
+    if (environ_cb(RETRO_ENVIRONMENT_GET_SYSTEM_DIRECTORY, &dir) && dir)
+    {
+        snprintf(retro_base_directory, sizeof(retro_base_directory), "%s", dir);
+    }
 
-   core = new GearboyCore();
-   core->Init();
+    core = new GearboyCore();
+    core->Init();
 
-   gearboy_frame_buf = new GB_Color[VIDEO_WIDTH * VIDEO_HEIGHT];
+    gearboy_frame_buf = new GB_Color[VIDEO_WIDTH * VIDEO_HEIGHT];
 
-   audio_sample_count = 0;
+    audio_sample_count = 0;
 }
 
 void retro_deinit(void)
 {
-   SafeDeleteArray(gearboy_frame_buf);
-   SafeDelete(core);
+    SafeDeleteArray(gearboy_frame_buf);
+    SafeDelete(core);
 }
 
 unsigned retro_api_version(void)
 {
-   return RETRO_API_VERSION;
+    return RETRO_API_VERSION;
 }
 
 void retro_set_controller_port_device(unsigned port, unsigned device)
 {
-   log_cb(RETRO_LOG_INFO, "Plugging device %u into port %u.\n", device, port);
+    log_cb(RETRO_LOG_INFO, "Plugging device %u into port %u.\n", device, port);
 }
 
 void retro_get_system_info(struct retro_system_info *info)
 {
-   memset(info, 0, sizeof(*info));
-   info->library_name     = "Gearboy";
-   info->library_version  = GEARBOY_VERSION;
-   info->need_fullpath    = true;
-   info->valid_extensions = "gb|dmg|gbc|cgb|sgb";
+    memset(info, 0, sizeof(*info));
+    info->library_name     = "Gearboy";
+    info->library_version  = GEARBOY_VERSION;
+    info->need_fullpath    = true;
+    info->valid_extensions = "gb|dmg|gbc|cgb|sgb";
 }
 
 static retro_video_refresh_t video_cb;
@@ -111,71 +111,61 @@ static retro_input_state_t input_state_cb;
 
 void retro_get_system_av_info(struct retro_system_av_info *info)
 {
-   float aspect                = (float)VIDEO_WIDTH/VIDEO_HEIGHT;
-
-   info->geometry.base_width   = VIDEO_WIDTH;
-   info->geometry.base_height  = VIDEO_HEIGHT;
-   info->geometry.max_width    = VIDEO_WIDTH;
-   info->geometry.max_height   = VIDEO_HEIGHT;
-   info->geometry.aspect_ratio = aspect;
-   info->timing.fps            = 4194304.0 / 70224.0;
-   info->timing.sample_rate    = 44100.0f;
-
+    float aspect                = (float)VIDEO_WIDTH/VIDEO_HEIGHT;
+    info->geometry.base_width   = VIDEO_WIDTH;
+    info->geometry.base_height  = VIDEO_HEIGHT;
+    info->geometry.max_width    = VIDEO_WIDTH;
+    info->geometry.max_height   = VIDEO_HEIGHT;
+    info->geometry.aspect_ratio = aspect;
+    info->timing.fps            = 4194304.0 / 70224.0;
+    info->timing.sample_rate    = 44100.0f;
 }
-
-static struct retro_rumble_interface rumble;
 
 void retro_set_environment(retro_environment_t cb)
 {
-   environ_cb = cb;
+    environ_cb = cb;
 
-   if (cb(RETRO_ENVIRONMENT_GET_LOG_INTERFACE, &logging))
-      log_cb = logging.log;
-   else
-      log_cb = fallback_log;
+    if (cb(RETRO_ENVIRONMENT_GET_LOG_INTERFACE, &logging))
+        log_cb = logging.log;
+    else
+        log_cb = fallback_log;
 
-   static const struct retro_controller_description controllers[] = {
-      { "Nintendo Gameboy", RETRO_DEVICE_SUBCLASS(RETRO_DEVICE_JOYPAD, 0) },
-   };
+    static const struct retro_controller_description controllers[] = {
+        { "Nintendo Gameboy", RETRO_DEVICE_SUBCLASS(RETRO_DEVICE_JOYPAD, 0) },
+    };
 
-   static const struct retro_controller_info ports[] = {
-      { controllers, 1 },
-      { NULL, 0 },
-   };
+    static const struct retro_controller_info ports[] = {
+        { controllers, 1 },
+        { NULL, 0 },
+    };
 
-   cb(RETRO_ENVIRONMENT_SET_CONTROLLER_INFO, (void*)ports);
+    cb(RETRO_ENVIRONMENT_SET_CONTROLLER_INFO, (void*)ports);
 }
 
 void retro_set_audio_sample(retro_audio_sample_t cb)
 {
-   audio_cb = cb;
+    audio_cb = cb;
 }
 
 void retro_set_audio_sample_batch(retro_audio_sample_batch_t cb)
 {
-   audio_batch_cb = cb;
+    audio_batch_cb = cb;
 }
 
 void retro_set_input_poll(retro_input_poll_t cb)
 {
-   input_poll_cb = cb;
+    input_poll_cb = cb;
 }
 
 void retro_set_input_state(retro_input_state_t cb)
 {
-   input_state_cb = cb;
+    input_state_cb = cb;
 }
 
 void retro_set_video_refresh(retro_video_refresh_t cb)
 {
-   video_cb = cb;
+    video_cb = cb;
 }
-
-static unsigned x_coord;
-static unsigned y_coord;
-static unsigned phase;
-static int mouse_rel_x;
-static int mouse_rel_y;
 
 void retro_reset(void)
 {
@@ -184,139 +174,138 @@ void retro_reset(void)
 
 static void update_input(void)
 {
-   input_poll_cb();
+    input_poll_cb();
 
-   if (input_state_cb(0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_UP))
-      core->KeyPressed(Up_Key);
-   else
-      core->KeyReleased(Up_Key);
-   if (input_state_cb(0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_DOWN))
-      core->KeyPressed(Down_Key);
-   else
-      core->KeyReleased(Down_Key);
-   if (input_state_cb(0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_LEFT))
-      core->KeyPressed(Left_Key);
-   else
-      core->KeyReleased(Left_Key);
-   if (input_state_cb(0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_RIGHT))
-      core->KeyPressed(Right_Key);
-   else
-      core->KeyReleased(Right_Key);
-   if (input_state_cb(0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_B))
-      core->KeyPressed(B_Key);
-   else
-      core->KeyReleased(B_Key);
-   if (input_state_cb(0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_A))
-      core->KeyPressed(A_Key);
-   else
-      core->KeyReleased(A_Key);
-   if (input_state_cb(0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_START))
-      core->KeyPressed(Start_Key);
-   else
-      core->KeyReleased(Start_Key);
-   if (input_state_cb(0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_SELECT))
-      core->KeyPressed(Select_Key);
-   else
-      core->KeyReleased(Select_Key);
-
+    if (input_state_cb(0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_UP))
+        core->KeyPressed(Up_Key);
+    else
+        core->KeyReleased(Up_Key);
+    if (input_state_cb(0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_DOWN))
+        core->KeyPressed(Down_Key);
+    else
+        core->KeyReleased(Down_Key);
+    if (input_state_cb(0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_LEFT))
+        core->KeyPressed(Left_Key);
+    else
+        core->KeyReleased(Left_Key);
+    if (input_state_cb(0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_RIGHT))
+        core->KeyPressed(Right_Key);
+    else
+        core->KeyReleased(Right_Key);
+    if (input_state_cb(0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_B))
+        core->KeyPressed(B_Key);
+    else
+        core->KeyReleased(B_Key);
+    if (input_state_cb(0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_A))
+        core->KeyPressed(A_Key);
+    else
+        core->KeyReleased(A_Key);
+    if (input_state_cb(0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_START))
+        core->KeyPressed(Start_Key);
+    else
+        core->KeyReleased(Start_Key);
+    if (input_state_cb(0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_SELECT))
+        core->KeyPressed(Select_Key);
+    else
+        core->KeyReleased(Select_Key);
 }
 
 static void check_variables(void)
 {
-
 }
 
 void retro_run(void)
 {
-   update_input();
+    update_input();
 
-   bool updated = false;
-   if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE_UPDATE, &updated) && updated)
-      check_variables();
+    bool updated = false;
+    if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE_UPDATE, &updated) && updated)
+        check_variables();
 
-   core->RunToVBlank(gearboy_frame_buf, audio_buf, &audio_sample_count);
+    core->RunToVBlank(gearboy_frame_buf, audio_buf, &audio_sample_count);
 
-   video_cb((uint8_t*)gearboy_frame_buf, VIDEO_WIDTH, VIDEO_HEIGHT, VIDEO_WIDTH * sizeof(GB_Color));
+    video_cb((uint8_t*)gearboy_frame_buf, VIDEO_WIDTH, VIDEO_HEIGHT, VIDEO_WIDTH * sizeof(GB_Color));
 
-   if (audio_sample_count > 0)
-      audio_batch_cb(audio_buf, audio_sample_count / 2);
+    if (audio_sample_count > 0)
+        audio_batch_cb(audio_buf, audio_sample_count / 2);
 
-   audio_sample_count = 0;
+    audio_sample_count = 0;
 }
 
 bool retro_load_game(const struct retro_game_info *info)
 {
-   core->LoadROM(info->path, false);
+    core->LoadROM(info->path, false);
 
-   struct retro_input_descriptor desc[] = {
-      { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_LEFT,   "Left" },
-      { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_UP,     "Up" },
-      { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_DOWN,   "Down" },
-      { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_RIGHT,  "Right" },
-      { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_START,  "Start" },
-      { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_SELECT, "Select" },
-      { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_B,      "B" },
-      { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_A,      "A" },
-      { 0 },
-   };
+    struct retro_input_descriptor desc[] = {
+        { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_LEFT,   "Left" },
+        { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_UP,     "Up" },
+        { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_DOWN,   "Down" },
+        { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_RIGHT,  "Right" },
+        { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_START,  "Start" },
+        { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_SELECT, "Select" },
+        { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_B,      "B" },
+        { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_A,      "A" },
+        { 0 },
+    };
 
-   environ_cb(RETRO_ENVIRONMENT_SET_INPUT_DESCRIPTORS, desc);
+    environ_cb(RETRO_ENVIRONMENT_SET_INPUT_DESCRIPTORS, desc);
 
-   enum retro_pixel_format fmt = RETRO_PIXEL_FORMAT_XRGB8888;
-   if (!environ_cb(RETRO_ENVIRONMENT_SET_PIXEL_FORMAT, &fmt))
-   {
-      log_cb(RETRO_LOG_INFO, "XRGB8888 is not supported.\n");
-      return false;
-   }
+    enum retro_pixel_format fmt = RETRO_PIXEL_FORMAT_XRGB8888;
+    if (!environ_cb(RETRO_ENVIRONMENT_SET_PIXEL_FORMAT, &fmt))
+    {
+        log_cb(RETRO_LOG_INFO, "XRGB8888 is not supported.\n");
+        return false;
+    }
 
-   snprintf(retro_game_path, sizeof(retro_game_path), "%s", info->path);
+    snprintf(retro_game_path, sizeof(retro_game_path), "%s", info->path);
 
-   check_variables();
+    check_variables();
 
-   (void)info;
-   return true;
+    bool achievements = true;
+    environ_cb(RETRO_ENVIRONMENT_SET_SUPPORT_ACHIEVEMENTS, &achievements);
+
+    return true;
 }
 
 void retro_unload_game(void)
 {
-
 }
 
 unsigned retro_get_region(void)
 {
-   return RETRO_REGION_NTSC;
+    return RETRO_REGION_NTSC;
 }
 
 bool retro_load_game_special(unsigned type, const struct retro_game_info *info, size_t num)
 {
-   return false;
+    return false;
 }
 
 size_t retro_serialize_size(void)
 {
-   return false;
+    return false;
 }
 
 bool retro_serialize(void *data_, size_t size)
 {
-   return false;
+    return false;
 }
 
 bool retro_unserialize(const void *data_, size_t size)
 {
-   return false;
+    return false;
 }
 
 void *retro_get_memory_data(unsigned id)
 {
     switch (id)
     {
-       case RETRO_MEMORY_SAVE_RAM:
-          return core->GetMemory()->GetCurrentRule()->GetRamBanks();
-       case RETRO_MEMORY_RTC:
-          return core->GetMemory()->GetCurrentRule()->GetRTCMemory();
-       case RETRO_MEMORY_SYSTEM_RAM:
-          return core->IsCGB() ? core->GetMemory()->GetCGBRAM() : core->GetMemory()->GetMemoryMap() + 0xC000;
+        case RETRO_MEMORY_SAVE_RAM:
+            return core->GetMemory()->GetCurrentRule()->GetRamBanks();
+        case RETRO_MEMORY_RTC:
+            return core->GetMemory()->GetCurrentRule()->GetRTCMemory();
+        case RETRO_MEMORY_SYSTEM_RAM:
+            return core->IsCGB() ? core->GetMemory()->GetCGBRAM() : core->GetMemory()->GetMemoryMap() + 0xC000;
     }
 
     return NULL;
@@ -338,11 +327,12 @@ size_t retro_get_memory_size(unsigned id)
 }
 
 void retro_cheat_reset(void)
-{}
+{
+}
 
 void retro_cheat_set(unsigned index, bool enabled, const char *code)
 {
-   (void)index;
-   (void)enabled;
-   (void)code;
+    (void)index;
+    (void)enabled;
+    (void)code;
 }
