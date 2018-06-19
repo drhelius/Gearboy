bf179f58d10e31b383445c67c60fa913996521b4
Ignacio Sanchez Gines <nacho@geardome.com>
2018-06-19 20:00:54 +0200

    clean project settings

--- platforms/ios/Gearboy/AppDelegate.mm
+++ platforms/ios/Gearboy/AppDelegate.mm
@@ -1,17 +1,17 @@
 /*
  * Gearboy - Nintendo Game Boy Emulator
  * Copyright (C) 2012  Ignacio Sanchez
- 
+
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
- 
+
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- 
+
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see http://www.gnu.org/licenses/
  *
@@ -32,6 +32,7 @@
     UINavigationController *navigationController = [splitViewController.viewControllers lastObject];
     navigationController.topViewController.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem;
     splitViewController.delegate = self;
+    splitViewController.presentsWithGesture = NO;
     masterViewController = (MasterViewController *)[[splitViewController.viewControllers firstObject] topViewController];
     return YES;
 }
@@ -73,10 +74,10 @@
         NSString *documentsDirectory = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
         NSError* error;
         NSFileManager *fileManager = [[NSFileManager alloc] init];
-        
+
         NSString* srcPath = [url path];
         NSString* destPath = [NSString stringWithFormat:@"%@/%@", documentsDirectory, [srcPath lastPathComponent]];
-        
+
         if ([fileManager copyItemAtPath:srcPath toPath:destPath error:&error])
         {
             if ([fileManager removeItemAtPath:[url path] error:&error])
@@ -89,10 +90,10 @@
                 return YES;
             }
         }
-        
+
         NSLog(@"ERROR %@", [error localizedDescription]);
     }
-    
+
     return NO;
 }
 
