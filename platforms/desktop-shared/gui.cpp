a6adafe8d38182779bf974d783f4f37bc7d9b2a9
Ignacio Sanchez Gines <nacho@geardome.com>
2020-04-05 23:44:58 +0200

    Disable matrix rendering for the momment

--- platforms/desktop-shared/gui.cpp
+++ platforms/desktop-shared/gui.cpp
@@ -285,7 +285,7 @@ static void main_menu(void)
             ImGui::MenuItem("Show FPS", "", &config_video.fps);
             ImGui::MenuItem("Bilinear Filtering", "", &config_video.bilinear);
             ImGui::MenuItem("Screen Ghosting", "", &config_video.mix_frames, false);
-            ImGui::MenuItem("Dot Matrix", "", &config_video.matrix, false);
+            //ImGui::MenuItem("Dot Matrix", "", &config_video.matrix, false);
             
             ImGui::Separator();
 
