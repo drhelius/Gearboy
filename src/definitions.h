ad72c0db4a42eb5d31a3328db750883df00defb9
Ignacio Sanchez Gines <nacho@geardome.com>
2020-04-07 23:07:08 +0200

    Bump version to 3.0.0

--- src/definitions.h
+++ src/definitions.h
@@ -31,7 +31,7 @@
 
 //#define DEBUG_GEARBOY 1
 #define GEARBOY_TITLE "Gearboy"
-#define GEARBOY_VERSION "3.0.0 - WIP"
+#define GEARBOY_VERSION "3.0.0"
 
 #ifndef EMULATOR_BUILD
 #define EMULATOR_BUILD "undefined"
