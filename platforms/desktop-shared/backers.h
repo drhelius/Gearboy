2c5b8b426332b3da43905ce796b8f7e7abc07444
Ignacio Sanchez Gines <nacho@geardome.com>
2022-09-27 18:56:37 +0200

    Update backers.h

--- platforms/desktop-shared/backers.h
+++ platforms/desktop-shared/backers.h
@@ -7,6 +7,7 @@ static const char * BACKERS_STR =
 "\n · Libretro / RetroArch team"
 "\n · Rupert Carmichael (carmiker)"
 "\n · Orange Kryptonite (OrangeKryptonite)"
+"\n · Danny Van den Heuve (Danny-VdH)"
 "\n · KPreston2900050";
 
 #endif	/* BACKERS_H */
