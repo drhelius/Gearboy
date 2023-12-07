5e7ebb6e7b912417f251f1939f1a474498644135
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2023-12-07 13:40:31 +0100

    Return proper error code if app fails to start

--- platforms/desktop-shared/main.cpp
+++ platforms/desktop-shared/main.cpp
@@ -23,7 +23,7 @@ int main(int argc, char* argv[])
 {
     int ret = application_init((argc == 2) ? argv[1] : NULL);
 
-    if (ret >= 0)
+    if (ret == 0)
         application_mainloop();
 
     application_destroy();    
