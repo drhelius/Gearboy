71af4c96be5d2a75dd21139869ed300dafb0aa4e
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2024-12-08 18:49:23 +0100

    Update libretro makefile

--- platforms/libretro/jni/Android.mk
+++ platforms/libretro/jni/Android.mk
@@ -8,9 +8,15 @@ include $(CORE_DIR)/Makefile.common
 
 COREFLAGS := -DHAVE_STDINT_H -DHAVE_INTTYPES_H -D__LIBRETRO__ -DGEARBOY_DISABLE_DISASSEMBLER
 
+GIT_VERSION ?= " $(shell git describe --abbrev=7 --dirty --always --tags || echo unknown)"
+ifneq ($(GIT_VERSION)," unknown")
+   COREFLAGS += -DEMULATOR_BUILD=\"$(GIT_VERSION)\"
+endif
+
 include $(CLEAR_VARS)
 LOCAL_MODULE    := retro
-LOCAL_SRC_FILES := $(SOURCES_CXX)
+LOCAL_SRC_FILES := $(SOURCES_CXX) $(SOURCES_C)
+LOCAL_CFLAGS    := $(COREFLAGS)
 LOCAL_CXXFLAGS  := $(COREFLAGS)
 LOCAL_LDFLAGS   := -Wl,-version-script=$(CORE_DIR)/link.T
 LOCAL_LDLIBS 	:= -llog
