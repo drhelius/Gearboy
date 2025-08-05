6657028e2c33b89635e31d7fd58bb9b61bd1cde0
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2025-08-05 20:36:14 +0200

    [desktop] remove duplicated code

--- platforms/desktop-shared/gui.h
+++ platforms/desktop-shared/gui.h
@@ -28,18 +28,6 @@
     #define EXTERN extern
 #endif
 
-#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
-#define BYTE_TO_BINARY_PATTERN_SPACED "%c%c%c%c %c%c%c%c"
-#define BYTE_TO_BINARY(byte)  \
-  (byte & 0x80 ? '1' : '0'), \
-  (byte & 0x40 ? '1' : '0'), \
-  (byte & 0x20 ? '1' : '0'), \
-  (byte & 0x10 ? '1' : '0'), \
-  (byte & 0x08 ? '1' : '0'), \
-  (byte & 0x04 ? '1' : '0'), \
-  (byte & 0x02 ? '1' : '0'), \
-  (byte & 0x01 ? '1' : '0') 
-
 enum gui_ShortCutEvent
 {
     gui_ShortcutOpenROM = 0,
