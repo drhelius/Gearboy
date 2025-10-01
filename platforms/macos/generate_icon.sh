babd3b665a196848d289a33083d86bd86fdd003b
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2025-10-01 13:03:48 +0200

    Update icon

---
+++ platforms/macos/generate_icon.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+input_filepath="image.png"
+output_iconset_name="iconfile.iconset"
+mkdir $output_iconset_name
+
+sips -z 16 16     $input_filepath --out "${output_iconset_name}/icon_16x16.png"
+sips -z 32 32     $input_filepath --out "${output_iconset_name}/icon_16x16@2x.png"
+sips -z 32 32     $input_filepath --out "${output_iconset_name}/icon_32x32.png"
+sips -z 64 64     $input_filepath --out "${output_iconset_name}/icon_32x32@2x.png"
+sips -z 128 128   $input_filepath --out "${output_iconset_name}/icon_128x128.png"
+sips -z 256 256   $input_filepath --out "${output_iconset_name}/icon_128x128@2x.png"
+sips -z 256 256   $input_filepath --out "${output_iconset_name}/icon_256x256.png"
+sips -z 512 512   $input_filepath --out "${output_iconset_name}/icon_256x256@2x.png"
+sips -z 512 512   $input_filepath --out "${output_iconset_name}/icon_512x512.png"
+
+iconutil -c icns $output_iconset_name
+
+rm -R $output_iconset_name
