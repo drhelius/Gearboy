748109a93a921ab25c0583407cd8233c10f8fccd
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2026-08-05 21:00:56 +0200

    Harden single-instance IPC file handling across emulators

--- platforms/shared/desktop/single_instance.cpp
+++ platforms/shared/desktop/single_instance.cpp
@@ -28,7 +28,6 @@
 #else
 #include <unistd.h>
 #include <fcntl.h>
-#include <signal.h>
 #include <sys/file.h>
 #include <sys/stat.h>
 #include <errno.h>
@@ -41,17 +40,13 @@
 
 static const char* k_lock_filename = ".lock";
 static const char* k_mailbox_filename = ".mailbox";
-static const char* k_mailbox_tmp_filename = ".mailbox.tmp";
 
 #if defined(_WIN32)
 static const char* k_event_name_prefix = "Local\\Gearboy_";
-#else
-static const char* k_pid_filename = ".pid";
 #endif
 
 static std::string s_lock_path;
 static std::string s_mailbox_path;
-static std::string s_mailbox_tmp_path;
 static bool s_is_primary = false;
 static bool s_initialized = false;
 static bool s_disabled = false;
@@ -65,91 +60,62 @@ static HANDLE s_event = NULL;
 static std::string s_event_name;
 #else
 static int s_lock_fd = -1;
-static std::string s_pid_path;
-static volatile sig_atomic_t s_signal_received = 0;
 #endif
 
 #if !defined(_WIN32)
-static void signal_handler(int sig)
+static bool is_private_file(int fd)
 {
-    if (sig == SIGUSR1)
-        s_signal_received = 1;
+    struct stat status;
+    if (fstat(fd, &status) != 0)
+        return false;
+
+    return S_ISREG(status.st_mode) &&
+           (status.st_uid == geteuid()) &&
+           ((status.st_mode & 077) == 0);
 }
 
-static bool is_temp_dir_accessible(const std::string& base_path)
+static bool create_runtime_directory(const char* app_name, std::string& path)
 {
-    std::string test_file = base_path + ".test";
-    FILE* f = fopen(test_file.c_str(), "w");
-    if (!f)
+    char directory[4096];
+    snprintf(directory, sizeof(directory), "/tmp/.%s-%u", app_name, (unsigned int)geteuid());
+
+    if (mkdir(directory, 0700) != 0 && errno != EEXIST)
         return false;
-    fclose(f);
-    unlink(test_file.c_str());
+
+    struct stat status;
+    if (lstat(directory, &status) != 0)
+        return false;
+
+    if (!S_ISDIR(status.st_mode) || status.st_uid != geteuid() || (status.st_mode & 077) != 0)
+        return false;
+
+    path = directory;
     return true;
 }
 #endif
 
-static bool is_lock_stale(void)
-{
-#if defined(_WIN32)
-    // On Windows, the lock is automatically released when the process exits
-    return false;
-#else
-    FILE* pid_file = fopen(s_pid_path.c_str(), "r");
-    if (!pid_file)
-        return true;
-
-    pid_t stored_pid = 0;
-    if (fscanf(pid_file, "%d", &stored_pid) != 1)
-    {
-        fclose(pid_file);
-        return true;
-    }
-    fclose(pid_file);
-
-    if (stored_pid <= 0)
-        return true;
-
-    // Check if process exists
-    if (kill(stored_pid, 0) == -1)
-    {
-        if (errno == ESRCH)
-            return true;
-    }
-
-    return false;
-#endif
-}
-
-static void cleanup_stale_lock(void)
-{
-#if !defined(_WIN32)
-    if (is_lock_stale())
-    {
-        Debug("Removing stale lock files");
-        unlink(s_lock_path.c_str());
-        unlink(s_pid_path.c_str());
-        unlink(s_mailbox_path.c_str());
-    }
-#endif
-}
-
-static void write_pid_file(void)
-{
-#if !defined(_WIN32)
-    FILE* pid_file = fopen(s_pid_path.c_str(), "w");
-    if (pid_file)
-    {
-        fprintf(pid_file, "%d", getpid());
-        fclose(pid_file);
-    }
-#endif
-}
-
 static bool read_mailbox(void)
 {
+#if defined(_WIN32)
     FILE* mailbox = fopen(s_mailbox_path.c_str(), "r");
-    if (!mailbox)
+#else
+    int mailbox_fd = open(s_mailbox_path.c_str(), O_RDONLY | O_NOFOLLOW);
+    if (mailbox_fd < 0 || !is_private_file(mailbox_fd))
+    {
+        if (mailbox_fd >= 0)
+            close(mailbox_fd);
         return false;
+    }
+
+    FILE* mailbox = fdopen(mailbox_fd, "r");
+#endif
+    if (!mailbox)
+    {
+#if !defined(_WIN32)
+        close(mailbox_fd);
+#endif
+        return false;
+    }
 
     s_pending_rom_path[0] = '\0';
     s_pending_symbol_path[0] = '\0';
@@ -209,8 +175,8 @@ void single_instance_init(const char* app_name)
     }
     std::string base_path = std::string(temp_path) + app_name + "_";
 #else
-    std::string base_path = std::string("/tmp/") + app_name + "_";
-    if (!is_temp_dir_accessible(base_path))
+    std::string base_path;
+    if (!create_runtime_directory(app_name, base_path))
     {
         Log("Single instance mode disabled: unable to access temp directory");
         s_disabled = true;
@@ -219,14 +185,11 @@ void single_instance_init(const char* app_name)
     }
 #endif
 
-    s_lock_path = base_path + k_lock_filename;
-    s_mailbox_path = base_path + k_mailbox_filename;
-    s_mailbox_tmp_path = base_path + k_mailbox_tmp_filename;
+    s_lock_path = base_path + "/" + k_lock_filename;
+    s_mailbox_path = base_path + "/" + k_mailbox_filename;
 
 #if defined(_WIN32)
     s_event_name = std::string(k_event_name_prefix) + app_name;
-#else
-    s_pid_path = base_path + k_pid_filename;
 #endif
 
     Debug("Single instance lock path: %s", s_lock_path.c_str());
@@ -255,8 +218,6 @@ void single_instance_destroy(void)
     {
         close(s_lock_fd);
         s_lock_fd = -1;
-        unlink(s_lock_path.c_str());
-        unlink(s_pid_path.c_str());
     }
 #endif
 
@@ -268,8 +229,6 @@ bool single_instance_try_lock(void)
 {
     if (!s_initialized || s_disabled)
         return true;
-
-    cleanup_stale_lock();
 
 #if defined(_WIN32)
     s_lock_file = CreateFileA(
@@ -323,10 +282,14 @@ bool single_instance_try_lock(void)
     return true;
 
 #else
-    s_lock_fd = open(s_lock_path.c_str(), O_CREAT | O_RDWR, 0600);
-    if (s_lock_fd < 0)
+    s_lock_fd = open(s_lock_path.c_str(), O_CREAT | O_RDWR | O_NOFOLLOW, 0600);
+    if (s_lock_fd < 0 || !is_private_file(s_lock_fd))
     {
+        if (s_lock_fd >= 0)
+            close(s_lock_fd);
         Error("Failed to open lock file: %s", strerror(errno));
+        s_lock_fd = -1;
+        s_disabled = true;
         s_is_primary = true;
         return true;
     }
@@ -342,16 +305,12 @@ bool single_instance_try_lock(void)
             return false;
         }
         Error("flock failed: %s", strerror(errno));
+        close(s_lock_fd);
+        s_lock_fd = -1;
+        s_disabled = true;
+        s_is_primary = true;
+        return true;
     }
-
-    write_pid_file();
-
-    // Set up signal handler
-    struct sigaction sa;
-    sa.sa_handler = signal_handler;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART;
-    sigaction(SIGUSR1, &sa, NULL);
 
     s_is_primary = true;
     Log("Single instance lock acquired");
@@ -369,8 +328,16 @@ void single_instance_send_message(const char* rom_path, const char* symbol_path)
     if (!s_initialized || s_disabled)
         return;
 
-    // Write to temp file first, then rename for atomicity
-    FILE* mailbox = fopen(s_mailbox_tmp_path.c_str(), "w");
+#if defined(_WIN32)
+    std::string mailbox_tmp_path = s_mailbox_path + ".tmp";
+    FILE* mailbox = fopen(mailbox_tmp_path.c_str(), "w");
+#else
+    std::string mailbox_tmp_path = s_mailbox_path + ".XXXXXX";
+    int mailbox_fd = mkstemp(&mailbox_tmp_path[0]);
+    FILE* mailbox = mailbox_fd >= 0 ? fdopen(mailbox_fd, "w") : NULL;
+    if (mailbox_fd >= 0 && !mailbox)
+        close(mailbox_fd);
+#endif
     if (!mailbox)
     {
         Error("Failed to write mailbox: %s", strerror(errno));
@@ -383,9 +350,9 @@ void single_instance_send_message(const char* rom_path, const char* symbol_path)
 
     // Atomic rename
 #if defined(_WIN32)
-    MoveFileExA(s_mailbox_tmp_path.c_str(), s_mailbox_path.c_str(), MOVEFILE_REPLACE_EXISTING);
+    MoveFileExA(mailbox_tmp_path.c_str(), s_mailbox_path.c_str(), MOVEFILE_REPLACE_EXISTING);
 #else
-    rename(s_mailbox_tmp_path.c_str(), s_mailbox_path.c_str());
+    rename(mailbox_tmp_path.c_str(), s_mailbox_path.c_str());
 #endif
 
     Log("Message sent to primary instance: ROM=%s, Symbols=%s",
@@ -404,25 +371,6 @@ void single_instance_send_message(const char* rom_path, const char* symbol_path)
     {
         Debug("Could not open event, primary will poll");
     }
-#else
-    // Read PID and send signal
-    FILE* pid_file = fopen(s_pid_path.c_str(), "r");
-    if (pid_file)
-    {
-        pid_t pid = 0;
-        if (fscanf(pid_file, "%d", &pid) == 1 && pid > 0)
-        {
-            if (kill(pid, SIGUSR1) == 0)
-            {
-                Debug("Signaled primary instance (PID %d) via SIGUSR1", pid);
-            }
-            else
-            {
-                Debug("Could not signal PID %d: %s", pid, strerror(errno));
-            }
-        }
-        fclose(pid_file);
-    }
 #endif
 }
 
@@ -440,23 +388,21 @@ void single_instance_poll(void)
         if (WaitForSingleObject(s_event, 0) == WAIT_OBJECT_0)
             should_check = true;
     }
-#else
-    // Check if we received a signal
-    if (s_signal_received)
-    {
-        s_signal_received = 0;
-        should_check = true;
-    }
 #endif
 
     if (!should_check)
     {
+#if defined(_WIN32)
         FILE* mailbox = fopen(s_mailbox_path.c_str(), "r");
         if (mailbox)
         {
             fclose(mailbox);
             should_check = true;
         }
+#else
+        if (access(s_mailbox_path.c_str(), F_OK) == 0)
+            should_check = true;
+#endif
     }
 
     if (should_check)
