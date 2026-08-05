5e66b9f787676689074d132c0c85201771309ff7
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2026-08-05 21:40:18 +0200

    Fix unsafe time conversion in core

--- src/common.h
+++ src/common.h
@@ -91,8 +91,15 @@ inline u32 pow_2_ceil(u32 n)
 
 inline void get_date_time_string(time_t timestamp, char* buffer, size_t size)
 {
-    struct tm* timeinfo = localtime(&timestamp);
-    strftime(buffer, size, "%Y-%m-%d %H:%M:%S", timeinfo);
+    struct tm time_info;
+#if defined(_WIN32)
+    if (localtime_s(&time_info, &timestamp) == 0)
+#else
+    if (localtime_r(&timestamp, &time_info) != NULL)
+#endif
+        strftime(buffer, size, "%Y-%m-%d %H:%M:%S", &time_info);
+    else if (size > 0)
+        buffer[0] = '\0';
 }
 
 inline void get_current_date_time_string(char* buffer, size_t size)
